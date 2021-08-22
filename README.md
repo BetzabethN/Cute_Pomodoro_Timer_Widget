c2611acf5fd48345d6851cb0a56d53e50c48fe3a
Betzabeth Narvaez <betzynarv@gmail.com>
2021-08-23 08:22:48 +0900

    added index, timer, and styling

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Cute_Pomodoro_Timer_Widget
+# Cute Pomodoro Timer Widget
+
+Cute pomodoro timer widget for Notion.
+
+## How to Use
+
+Using the `/embed` block, in the link section, enter the github pages link of this widget.
---
+++ timer.js
@@ -0,0 +1,97 @@
+var minutes;
+var seconds;
+var ch_button;
+const p_title =  document.querySelector('#pomodoro-title');
+
+
+function study(callback) {
+    minutes = 25;
+    seconds = 0;
+    p_title.innerText = "Study or Work!";
+    document.getElementById("pomodoro-pic").src = "https://cute-pomodoro-timer-widget.s3.us-east-2.amazonaws.com/in-session.png";
+    
+
+    var interval = setInterval(
+        function() {
+            var el = document.getElementById("timer");
+            // if countdown in done, clear it, and start rest period
+            if (seconds == 0 && minutes == 0) {
+                clearInterval(interval);
+                el.innerHTML = "00:00";
+                callback(); 
+            }
+            // if seconds is done, minus a minute
+            else if (seconds == 0) {
+                minutes = minutes - 1;
+                seconds = 59;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+
+            // as long as there is seconds left, subtract seconds
+            else if (seconds >= 11) {
+                seconds = seconds - 1;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+
+             // add leading zeros to seconds smaller than 10
+            else if (seconds <= 10) {
+                seconds = seconds - 1;
+                el.innerHTML = minutes + ":0" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":0" + seconds;
+            }
+        },
+        1000
+    )
+       
+};
+
+function rest() {
+    minutes = 5;
+    seconds = 0;
+    p_title.innerText = "Rest!";
+    document.getElementById("pomodoro-pic").src = "https://cute-pomodoro-timer-widget.s3.us-east-2.amazonaws.com/rest.png";
+
+
+
+    var interval = setInterval(
+        function() {
+            var el = document.getElementById("timer");
+            // if countdown in done, clear it, and alert
+            if (seconds == 0 && minutes == 0) {
+                clearInterval(interval);
+                el.innerHTML = "00:00";
+                p_title.innerText = "Ready to start again?";
+                document.getElementById("pomodoro-pic").src = "https://cute-pomodoro-timer-widget.s3.us-east-2.amazonaws.com/pause-session.png";
+            }
+            // if seconds is done, minus a minute
+            else if (seconds == 0) {
+                minutes = minutes - 1;
+                seconds = 59;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+           // as long as there is seconds left, subtract seconds
+           else if (seconds >= 11) {
+                seconds = seconds - 1;
+                el.innerHTML = minutes + ":" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":" + seconds;
+            }
+
+            // add leading zeros to seconds smaller than 10
+            else if (seconds <= 10) {
+                seconds = seconds - 1;
+                el.innerHTML = minutes + ":0" + seconds;
+                // change doc title so if in another tab, can still see time left
+                document.title = minutes + ":0" + seconds;
+            }
+        },
+        1000
+    )
+};
